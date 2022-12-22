deb74831744118fc933fb8822dd63f71b4efea2f
snehal patel <snpatel.dev@gmail.com>
2022-12-22 13:17:58 -0500

    #7 resuable table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent {
+  data = [
+    { name: 'James', age: 24, job: 'Engineer' },
+    { name: 'Jill', age: 26, job: 'Designer' },
+    { name: 'Jack', age: 28, job: 'Engineer' },
+  ];
 
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
 }
