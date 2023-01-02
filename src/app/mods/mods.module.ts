bca44c9e4cfbaf22fd03ec00f4bed6c54a66bf68
snehal patel <snpatel.dev@gmail.com>
2023-01-01 22:41:22 -0500

    #12 mods home component

--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ModsRoutingModule } from './mods-routing.module';
+import { ModsHomeComponent } from './mods-home/mods-home.component';
+import { ModalComponent } from './modal/modal.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ModsHomeComponent,
+    ModalComponent
+  ],
   imports: [
     CommonModule,
     ModsRoutingModule
